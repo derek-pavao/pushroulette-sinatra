e9bf16f49233aa469ac2916d903ce3d2fd6bd360
Derek Pavao <dpavao@astonish.com>
2014-09-23 16:44:44 -0400

    adding the beginning of an install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="MAC"
+    DEPS=0
+
+    if ! which brew > /dev/null; then
+        echo "================================================"
+        echo "Pushroulette requires 'Homebrew' to be installed"
+        echo 'Homebrew can be insatlled with ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"'
+        echo "================================================"
+        ((DEPS++))
+    fi
+    if ! which git > /dev/null; then
+        echo "================================================"
+        echo "Pushroulette requires 'git' to be installed"
+        echo "Git can be installed with \"[sudo] brew install git\""
+        echo "================================================"
+        ((DEPS++))
+    fi
+    if ! which bundle > /dev/null; then
+        echo "================================================"
+        echo "Pushroulette requires 'bundler' to be installed"
+        echo "Bundler can be installed with \"[sudo] gem install bundler\""
+        echo "================================================"
+        ((DEPS++))
+    fi
+    if [[ "$DEPS" != 0 ]]; then
+        echo ""
+        echo "You are missing the $DEPS dependency or depencencies above, please install them and rerun this command"
+        echo ""
+        exit
+    fi
+
+    git clone https://github.com/Astonish-Results/pushroulette-sinatra.git ./
+
+    if ! gem install bundler &> /dev/null; then
+        sudo gem install bundler
+    fi
+    bundle install
+    brew install libav --with-libvorbis --with-sdl --with-theora
+
+
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    OS="LINUX"
+else
+    echo "PUSH ROULETTE DOES NOT SUPPORT YOUR OPERATING SYSTEM"
+fi
+
+echo "$OS"
